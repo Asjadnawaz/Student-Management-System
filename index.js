e23b46813415d5c55c9929fc43bd9b1887523869
Asjad Nawaz Khan <asjadnawaz2002@gmail.com>
2024-06-13 23:43:35 +0500

    Updated

---
+++ index.js
@@ -0,0 +1,178 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+class Student {
+    static counter = 1000;
+    id;
+    name;
+    courses;
+    balance;
+    constructor(name) {
+        this.id = Student.counter++;
+        this.name = name;
+        this.courses = [];
+        this.balance = 56000;
+    }
+    //Method to Enroll a student in a Course
+    enroll_course(course) {
+        this.courses.push(course);
+    }
+    view_balance() {
+        console.log(`Dues Remaining for ${this.name} : Rs.${this.balance}`);
+    }
+    //method to pay student fees
+    pay_fees(amount) {
+        this.balance -= amount;
+        console.log(`Rs.${amount} Fees paid successfully for ${this.name}`);
+    }
+    show_status() {
+        console.log(`\n`);
+        console.log(`ID: ${this.id}`);
+        console.log(`Name: ${this.name}`);
+        console.log(`Courses: ${this.courses}`);
+        console.log(`Balance: Rs.${this.balance}`);
+    }
+}
+// 2nd Class
+class Student_manager {
+    students;
+    constructor() {
+        this.students = [];
+    }
+    //Method to add a new student
+    add_student(name) {
+        let student = new Student(name);
+        this.students.push(student);
+        console.log(`Student "${chalk.greenBright(name)}" added successfully.
+Student ID: ${chalk.green(student.id)}`);
+    }
+    enroll_student(student_id, course) {
+        let student = this.find_student(student_id);
+        if (student) {
+            student.enroll_course(course);
+            console.log(`${student.name} enrolled in ${course} successfully.`);
+        }
+    }
+    view_student_balance(student_id) {
+        let student = this.find_student(student_id);
+        if (student) {
+            student.view_balance();
+        }
+        else {
+            console.log(`Student not found. Please Enter a correct student ID.`);
+        }
+    }
+    // Method to pay student fees
+    pay_student_fees(student_id, amount) {
+        let student = this.find_student(student_id);
+        if (student) {
+            student.pay_fees(amount);
+        }
+        else {
+            console.log(`Student not Found. Please enter a correct student ID`);
+        }
+    }
+    //Method to display student status
+    show_student_status(student_id) {
+        let student = this.find_student(student_id);
+        if (student) {
+            student.show_status();
+        }
+    }
+    find_student(student_id) {
+        return this.students.find((std) => std.id === student_id);
+    }
+}
+//Main Function to run the program
+async function main() {
+    console.log(`Student Management System made By Asjad Nawaz`);
+    console.log("=".repeat(50));
+    let student_manager = new Student_manager();
+    // While loop to keep program running
+    let exit = false;
+    while (!exit) {
+        let choice = await inquirer.prompt([
+            {
+                name: "choice",
+                type: "list",
+                message: "Select an Option",
+                choices: [
+                    "Add Student",
+                    "Enroll Student",
+                    "View Student Balance",
+                    "Pay Fees",
+                    "Show Status",
+                    "Exit",
+                ],
+            },
+        ]);
+        // Using switch case to handled user choice
+        switch (choice.choice) {
+            case "Add Student":
+                let name_input = await inquirer.prompt([
+                    {
+                        name: "name",
+                        type: "input",
+                        message: "Enter a Student Name",
+                    },
+                ]);
+                student_manager.add_student(name_input.name);
+                break;
+            case "Enroll Student":
+                let course_input = await inquirer.prompt([
+                    {
+                        name: "student_id",
+                        type: "number",
+                        message: "Enter a Student ID",
+                    },
+                    {
+                        name: "course",
+                        type: "input",
+                        message: "Enter a Course Name",
+                    },
+                ]);
+                student_manager.enroll_student(course_input.student_id, course_input.course);
+                break;
+            case "View Student Balance":
+                let balance_input = await inquirer.prompt([
+                    {
+                        name: "student_id",
+                        type: "number",
+                        message: "Enter a Student ID",
+                    },
+                ]);
+                student_manager.view_student_balance(balance_input.student_id);
+                break;
+            case "Pay Fees":
+                let fees_input = await inquirer.prompt([
+                    {
+                        name: "student_id",
+                        type: "number",
+                        message: "Enter a Student ID",
+                    },
+                    {
+                        name: "amount",
+                        type: "number",
+                        message: "Enter the amount to pay",
+                    },
+                ]);
+                student_manager.pay_student_fees(fees_input.student_id, fees_input.amount);
+                break;
+            case "Show Status":
+                let status_input = await inquirer.prompt([
+                    {
+                        name: "student_id",
+                        type: "number",
+                        message: "Enter a Student ID",
+                    },
+                ]);
+                student_manager.show_student_status(status_input.student_id);
+                break;
+            case "Exit":
+                console.log(`Exiting....`);
+                exit = true;
+                setTimeout(() => { process.exit(); }, 1000);
+                break;
+        }
+    }
+}
+main();
